6ba58a3732c34f54fd6056877ce720a79fe4bea9
Vyness6 <vanessahhope@gmail.com>
2025-04-30 08:45:32 +0300

    submission for week 7 assignment

---
+++ answers.sql
@@ -0,0 +1,26 @@
+--Question 1--
+SELECT 101 AS OrderID, 'John Doe' AS CustomerName, 'Laptop' AS Product
+UNION ALL
+SELECT 101, 'John Doe', 'Mouse'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Tablet'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Keyboard'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Mouse'
+UNION ALL
+SELECT 103, 'Emily Clark', 'Phone';
+
+--Question 2--
+SELECT DISTINCT 
+     orderLineNumber
+    CustomerName
+FROM 
+    OrderDetails;
+
+    SELECT 
+    orderLineNumber, 
+    productCode, 
+    quantityOrdered
+FROM 
+    OrderDetails;
